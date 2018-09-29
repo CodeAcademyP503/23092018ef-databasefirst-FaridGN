df2b244f3790a47d3cc53c2323387c6d989df4a3
FaridGN <37265938+FaridGN@users.noreply.github.com>
2018-09-29 12:55:58 +0400

    Add files via upload

---
+++ LoanApp/LoanApp/Table.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using LoanApp;
+
+namespace LoanApp
+{
+    public partial class Table : Form
+    {
+        public Table()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+           
+            
+        }
+
+        private void Table_Load(object sender, EventArgs e)
+        {
+            using (LoanAppEntities lapp = new LoanAppEntities())
+            {
+                dataGridView1.DataSource = lapp.Allviews.ToList();
+
+                lapp.SaveChanges();
+
+            }
+        }
+    }
+}
